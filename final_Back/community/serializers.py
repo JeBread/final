510f92336f39bcc939f6263e9d93114cc650b222
김한결 <kmr5326@naver.com>
2023-05-19 12:17:46 +0900

    게시판 댓글 생성, 게시글 좋아요

--- final_Back/community/serializers.py
+++ final_Back/community/serializers.py
@@ -1,13 +1,7 @@
 from rest_framework import serializers
 from .models import *
 
-class ArticleListSerializer(serializers.ModelSerializer):
-    class Meta:
-        model=Article
-        fields='__all__'
-        
-
-
+     
 class ArticleDetailSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
@@ -29,3 +23,28 @@ class ArticleDetailSerializer(serializers.ModelSerializer):
         fields='__all__'
         read_only_fields=('like_users','user')
 
+class CommentSerializer(serializers.ModelSerializer):
+    def create(self, validated_data):
+    
+        request = self.context.get("request")
+        
+        comment = Comment()
+        comment.content = validated_data['content']
+        comment.article_id=self.context.get("article")
+        comment.user = request.user
+
+        comment.save()
+
+        return comment
+
+    class Meta:
+        
+        model=Comment
+        fields='__all__'
+        read_only_fields=('article_id','user')
+
+class ArticleListSerializer(serializers.ModelSerializer):
+    comment_set=CommentSerializer(many=True,read_only=True)
+    class Meta:
+        model=Article
+        fields='__all__'
