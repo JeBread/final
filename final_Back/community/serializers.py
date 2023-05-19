c0f9f5845536a2d72cec90406064c55e523fafce
김한결 <kmr5326@naver.com>
2023-05-19 15:27:56 +0900

    community serializer 수정

--- final_Back/community/serializers.py
+++ final_Back/community/serializers.py
@@ -1,29 +1,15 @@
 from rest_framework import serializers
 from .models import *
+from django.contrib.auth import get_user_model
 
-     
-class ArticleDetailSerializer(serializers.ModelSerializer):
-
-    def create(self, validated_data):
-    
-        request = self.context.get("request")
-        
-        article = Article()
-        article.title = validated_data['title']
-        article.content = validated_data['content']
-        article.user = request.user
-
-        article.save()
-
-        return article
-
+class UserSerializer(serializers.ModelSerializer):
     class Meta:
-        
-        model=Article
+        model=get_user_model()
         fields='__all__'
-        read_only_fields=('like_users','user')
+        
 
 class CommentSerializer(serializers.ModelSerializer):
+    username=serializers.CharField(source='user.username')
     def create(self, validated_data):
     
         request = self.context.get("request")
@@ -43,8 +29,32 @@ class CommentSerializer(serializers.ModelSerializer):
         fields='__all__'
         read_only_fields=('article_id','user')
 
+class ArticleDetailSerializer(serializers.ModelSerializer):
+    comment_set=CommentSerializer(many=True,read_only=True)
+    username=serializers.CharField(source='user.username')
+    like_users_count=serializers.IntegerField(source='like_users.count')
+    def create(self, validated_data):
+    
+        request = self.context.get("request")
+        
+        article = Article()
+        article.title = validated_data['title']
+        article.content = validated_data['content']
+        article.user = request.user
+
+        article.save()
+
+        return article
+
+    class Meta:
+        
+        model=Article
+        fields='__all__'
+        read_only_fields=('like_users','user')
+
 class ArticleListSerializer(serializers.ModelSerializer):
     comment_set=CommentSerializer(many=True,read_only=True)
+    like_users_count=serializers.IntegerField(source='like_users.count')
     class Meta:
         model=Article
         fields='__all__'
