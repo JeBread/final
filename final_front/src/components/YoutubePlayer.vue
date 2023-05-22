675ceb762db32d445f2c04174a52676637b00638
김한결 <kmr5326@naver.com>
2023-05-22 10:55:54 +0900

    quiz 기능만 구현

---
+++ final_front/src/components/YoutubePlayer.vue
@@ -0,0 +1,52 @@
+<template>
+    <div>
+        <youtube :video-id="videoId" ref="youtube" style="display:none;"></youtube>
+        <button @click="playVideo">재생</button>
+        <button @click="pauseVideo">일시정지</button>
+    </div>
+  </template>
+
+<script>
+import Vue from 'vue'
+import VueYoutube from 'vue-youtube'
+ 
+Vue.use(VueYoutube)
+
+export default {
+  name: "YoutubePlayer",
+  props:{
+    videoId:String
+  },
+  data() {
+    return {
+        
+      playerVars: {
+        autoplay: 1
+      }
+    }
+  },
+  components: {
+    
+  },
+  computed: {
+    player() {
+      return this.$refs.youtube.player
+    }
+  },
+  methods:{
+    playVideo() {
+      this.player.playVideo()
+    },
+    pauseVideo() {
+      this.player.pauseVideo()
+    },
+  }
+};
+</script>
+
+<style scoped>
+iframe {
+  width: 0px;
+  height: 0px;
+}
+</style>
