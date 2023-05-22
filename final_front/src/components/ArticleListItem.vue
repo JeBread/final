a0dfde0b2a80290d614658b88b2506351252e38a
상제 <sangje112@gmail.com>
2023-05-22 11:49:33 +0900

    영화 목록 수정

--- final_front/src/components/ArticleListItem.vue
+++ final_front/src/components/ArticleListItem.vue
@@ -1,13 +1,17 @@
 <template>
   <div>
+    <h5>{{ article.id }}</h5>
     <router-link
       :to="{
         name: 'ArticleDetailView',
         params: { articleId: article.id },
       }"
     >
-      게시글 상세 정보
+      <p>게시글 제목 : {{ article.title }}</p>
     </router-link>
+    <p>작성자 : {{ article.user }}</p>
+    <p>게시글 내용 : {{ article.content }}</p>
+    <p>좋아요 : {{ article.like_users_count }}</p>
   </div>
 </template>
 
--- final_front/src/components/MovieListItem.vue
+++ final_front/src/components/MovieListItem.vue
@@ -1,16 +1,27 @@
 <template>
-  <div>
-    <h5>{{ movie.id }}</h5>
-    <p>{{ movie.title }}</p>
-    <router-link
-      :to="{
-        name: 'DetailView',
-        params: { id: movie.id },
-      }"
-    >
-      [DETAIL]
-    </router-link>
-    <hr />
+  <div class="movies container">
+    <h1>영화 페이지</h1>
+    <div class="carousel-wrapper">
+      <div class="carousel">
+        <div class="wrapper">
+          <div class="carousel-content">
+            <h5>{{ movie.id }}</h5>
+            <img :src="poster_url" alt="" width="200" />
+            <p>{{ movie.title }}</p>
+            <p>{{ movie.vote_average }}</p>
+            <router-link
+              :to="{
+                name: 'DetailView',
+                params: { id: movie.id },
+              }"
+            >
+              [DETAIL]
+            </router-link>
+            <hr />
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -19,6 +30,12 @@ export default {
   name: "MovieListItem",
   props: {
     movie: Object,
+  },
+  data() {
+    return {
+      poster_url:
+        "https://image.tmdb.org/t/p/original" + this.movie.poster_path,
+    };
   },
 };
 </script>
