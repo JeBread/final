d2a67e951ec14a79f405aff5b3f4506401b58496
김한결 <kmr5326@naver.com>
2023-05-23 16:47:15 +0900

    ost페이지,게시글 삭제,댓글 삭제 구현

--- final_Back/community/urls.py
+++ final_Back/community/urls.py
@@ -9,4 +9,6 @@ urlpatterns = [
     path('<int:article_pk>/', views.detail, name='detail'),
     path('<int:article_pk>/comments/create/', views.create_comment, name='create_comment'),
     path('<int:article_pk>/like/', views.like, name='like'),
+    path('<int:article_pk>/delete/',views.article_delete),
+    path('comments/<int:comment_pk>/delete/',views.comment_delete),
 ]
---
+++ final_front/src/components/OstViewPlayer.vue
@@ -0,0 +1,50 @@
+<template>
+    <div>
+        <youtube :video-id="videoId" ref="youtube" style="display: none;"></youtube>
+    </div>
+  </template>
+
+<script>
+import Vue from 'vue'
+import VueYoutube from 'vue-youtube'
+ 
+Vue.use(VueYoutube)
+
+export default {
+  name: "OstViewPlayer",
+  props:{
+    videoId:String
+  },
+  data() {
+    return {
+    
+      playerVars: {
+        autoplay: 1
+      }
+    }
+  },
+  components: {
+    
+  },
+  computed: {
+    player() {
+      return this.$refs.youtube.player
+    }
+  },
+  methods:{
+    playVideo() {
+      this.player.playVideo()
+    },
+    pauseVideo() {
+      this.player.pauseVideo()
+    },
+  }
+};
+</script>
+
+<style scoped>
+iframe {
+  width: 0px;
+  height: 0px;
+}
+</style>
