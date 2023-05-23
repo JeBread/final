32a4f62103929a3a13ef82a683128f814ffeb044
상제 <sangje112@gmail.com>
2023-05-23 10:29:00 +0900

    navbar & login page design

--- final_front/src/App.vue
+++ final_front/src/App.vue
@@ -1,29 +1,74 @@
 <template>
-  <div class="background" id="app">
-    <nav class="d-flex justify-content-between">
-      <img src="./assets/logo.png" alt="" id="logo" @click="toHome" />
-      <div>
-        <router-link v-if="token" :to="{ name: 'MovieView' }"
-          >영화 목록</router-link
-        ><span v-if="token"> | </span>
-        <router-link v-if="token" :to="{ name: 'QuizView' }">퀴즈</router-link>
-        <span v-if="token"> | </span>
-        <router-link v-if="token" :to="{ name: 'OstView' }"
-          >Ost</router-link
-        ><span v-if="token"> | </span>
-        <router-link v-if="token" :to="{ name: 'RecommendedView' }"
-          >영화 추천</router-link
-        ><span v-if="token"> | </span>
-        <router-link v-if="token" :to="{ name: 'CommunityView' }"
-          >커뮤니티</router-link
-        ><span v-if="token"> | </span>
-        <router-link v-if="!token" :to="{ name: 'SignUpView' }"
-          >회원가입</router-link
-        >
-        <button v-if="token" class="logout" @click="logout">로그아웃</button>
-      </div>
-    </nav>
-    <router-view />
+  <div
+    class="first leading-normal tracking-normal text-indigo-400 m-6 background"
+    id="app"
+  >
+    <div class="h-full">
+      <!-- Nav Bar -->
+      <nav class="w-full container mx-auto">
+        <div class="w-full flex items-center justify-between">
+          <a
+            class="flex items-center text-indigo-400 no-underline hover:no-underline font-bold text-2xl lg:text-4xl mt-5"
+            href="#"
+            @click="toHome"
+          >
+            Rain<span
+              class="bg-clip-text text-transparent bg-gradient-to-r from-green-400 via-pink-500 to-purple-500"
+              >blur</span
+            >
+          </a>
+          <!-- <img src="./assets/logo.png" alt="" id="logo" @click="toHome" /> -->
+          <!-- nav bar right section-->
+          <div class="flex w-1/2 justify-end content-center mt-5 font-semibold">
+            <router-link
+              v-if="token"
+              :to="{ name: 'MovieView' }"
+              class="inline-block text-blue-300 no-underline hover:text-pink-500 hover:text-underline text-center h-10 p-2 md:h-auto md:p-4 transform hover:scale-125 duration-300 ease-in-out fill-current h-6"
+              >Movie</router-link
+            >
+            <router-link
+              v-if="token"
+              :to="{ name: 'QuizView' }"
+              class="inline-block text-blue-300 no-underline hover:text-pink-500 hover:text-underline text-center h-10 p-2 md:h-auto md:p-4 transform hover:scale-125 duration-300 ease-in-out fill-current h-6"
+              >Quiz</router-link
+            >
+
+            <router-link
+              v-if="token"
+              :to="{ name: 'OstView' }"
+              class="inline-block text-blue-300 no-underline hover:text-pink-500 hover:text-underline text-center h-10 p-2 md:h-auto md:p-4 transform hover:scale-125 duration-300 ease-in-out fill-current h-6"
+              >Ost</router-link
+            >
+            <router-link
+              v-if="token"
+              :to="{ name: 'RecommendedView' }"
+              class="inline-block text-blue-300 no-underline hover:text-pink-500 hover:text-underline text-center h-10 p-2 md:h-auto md:p-4 transform hover:scale-125 duration-300 ease-in-out fill-current h-6"
+              >Recommend</router-link
+            >
+            <router-link
+              v-if="token"
+              :to="{ name: 'CommunityView' }"
+              class="inline-block text-blue-300 no-underline hover:text-pink-500 hover:text-underline text-center h-10 p-2 md:h-auto md:p-4 transform hover:scale-125 duration-300 ease-in-out fill-current h-6"
+              >Community</router-link
+            >
+            <router-link
+              v-if="!token"
+              :to="{ name: 'SignUpView' }"
+              class="inline-block text-blue-300 no-underline hover:text-pink-500 hover:text-underline text-center h-10 p-2 md:h-auto md:p-4 transform hover:scale-125 duration-300 ease-in-out fill-current h-6"
+              >SignUp</router-link
+            >
+            <button
+              v-if="token"
+              class="inline-block text-blue-300 no-underline hover:text-pink-500 hover:text-underline text-center h-10 p-2 md:h-auto md:p-4 transform hover:scale-125 duration-300 ease-in-out fill-current h-6"
+              @click="logout"
+            >
+              LogOut
+            </button>
+          </div>
+        </div>
+      </nav>
+      <router-view class="mt-32" />
+    </div>
   </div>
 </template>
 
@@ -52,11 +97,20 @@ export default {
 
 <style>
 @font-face {
-  font-family: "Riders-font";
-  src: url("./assets/fonts/BMHANNAAir_ttf.ttf");
+  font-family: "Poppins", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto,
+    "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji",
+    "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji";
+  src: url("https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap");
 }
-/* html,
-body {
+
+@import url("https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap");
+
+.first {
+  font-family: "Poppins", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto,
+    "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji",
+    "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji";
+}
+/* html, body {
   margin: 0;
   padding: 0;
   height: 100vh;
@@ -71,12 +125,11 @@ body {
 } */
 #app {
   /* font-family: 'Gowun Dodum', sans-serif; */
-  font-family: "Riders-font";
+  /* font-family: "Riders-font";
   -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+  -moz-osx-font-smoothing: grayscale; */
   text-align: center;
-  color: white;
-  height: 100%;
+  /* color: white; */
   /* overflow: hidden; */
 }
 
@@ -87,52 +140,52 @@ body {
   background-position: center;
   height: 100vh;
   margin: 0;
-  overflow: hidden;
+  overflow: auto;
 }
 
-nav {
+/* nav {
   padding: 10px 150px 30px;
   line-height: 80px;
-}
+} */
 
 /* nav a {
   font-weight: bold;
   color: #2c3e50;
 } */
-nav a {
+/* nav a {
   font-weight: bold;
   color: rgb(166 229 227);
   text-decoration: none;
-}
-nav a:hover {
+} */
+/* nav a:hover {
   color: #0d6efd;
-}
+} */
 
 /* nav a.router-link-exact-active {
   color: #42b983;
 } */
-nav a.router-link-exact-active {
+/* nav a.router-link-exact-active {
   color: #2c3e50;
-}
-.logout {
+} */
+/* .logout {
   font-weight: bold;
   color: rgb(166 229 227);
   text-decoration: none;
   cursor: pointer;
-}
-.logout:hover {
+} */
+/* .logout:hover {
   color: #0d6efd;
-}
+} */
 
-#logo {
+/* #logo {
   width: 50px;
   height: 50px;
-}
+} */
 
-#logo:hover {
+/* #logo:hover {
   animation: rotate_image 3s linear infinite;
   transform-origin: 50% 50%;
-}
+} */
 
 @keyframes rotate_image {
   100% {
--- final_front/src/views/LogInView.vue
+++ final_front/src/views/LogInView.vue
@@ -1,15 +1,71 @@
 <template>
   <div class="login-page">
-    <h1 class="fw-bold">로그인 페이지</h1>
-    <form @submit.prevent="login">
-      <label for="username">username : </label>
-      <input type="text" id="username" v-model="username" /><br />
+    <!-- Main -->
+    <div
+      class="container pt-24 md:pt-36 mx-auto flex flex-wrap flex-col md:flex-row items-center"
+    >
+      <!-- Left Col -->
+      <div
+        class="flex flex-col w-full xl:w-2/5 justify-center lg:items-start overflow-y-hidden"
+      >
+        <h1
+          class="my-4 text-3xl md:text-5xl text-white opacity-75 font-bold leading-tight text-center md:text-left"
+        >
+          Main
+          <span
+            class="bg-clip-text text-transparent bg-gradient-to-r from-green-400 via-pink-500 to-purple-500"
+          >
+            Hero Message
+          </span>
+          to sell yourself!
+        </h1>
+        <p
+          class="leading-normal text-base md:text-2xl mb-8 text-center md:text-left"
+        >
+          Sub-hero message, not too long and not too short. Make it just right!
+        </p>
+        <!-- 로그인 form -->
+        <form
+          class="bg-gray-900 opacity-75 w-full shadow-lg rounded-lg px-8 pt-6 pb-8 mb-4"
+          @submit.prevent="login"
+        >
+          <div class="mb-4">
+            <label
+              class="block text-blue-300 py-2 font-bold mb-2"
+              for="username"
+            >
+            </label>
+            <input
+              class="shadow appearance-none border rounded w-full p-3 text-gray-700 leading-tight focus:ring transform transition hover:scale-105 duration-300 ease-in-out"
+              type="text"
+              id="username"
+              placeholder="Name"
+              v-model="username"
+            /><br />
 
-      <label for="password"> password : </label>
-      <input type="password" id="password" v-model="password" />
-
-      <input type="submit" value="logIn" />
-    </form>
+            <label
+              class="block text-blue-300 py-2 font-bold mb-2"
+              for="password"
+            >
+            </label>
+            <input
+              class="shadow appearance-none border rounded w-full p-3 text-gray-700 leading-tight focus:ring transform transition hover:scale-105 duration-300 ease-in-out"
+              type="password"
+              id="password"
+              placeholder="Password"
+              v-model="password"
+            />
+            <div class="flex items-center justify-between pt-4">
+              <input
+                class="bg-gradient-to-r from-purple-800 to-green-500 hover:from-pink-500 hover:to-green-500 text-white font-bold py-2 px-4 rounded focus:ring transform transition hover:scale-105 duration-300 ease-in-out"
+                type="submit"
+                value="logIn"
+              />
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
   </div>
 </template>
 
