f38918003a6deb7911c190249328cc78012ff59d
상제 <sangje112@gmail.com>
2023-05-23 14:14:54 +0900

    movie card 수정중

--- final_front/src/components/MovieList.vue
+++ final_front/src/components/MovieList.vue
@@ -1,7 +1,24 @@
 <template>
-  <div class="movie-list">
-    <h3>Movie List</h3>
-    <MovieListItem v-for="movie in movies" :key="movie.id" :movie="movie" />
+  <div class="movie-list" style="margin-left: 10%; margin-right: 10%">
+    <div class="MovieView">
+      <div class="title" style="text-align: center; margin-top: 40px">
+        Movie List
+      </div>
+      <hr />
+      <div class="container-fluid" style="margin-top: 40px">
+        <Flicking
+          class="slide"
+          :options="{ circular: true }"
+          :plugins="plugins"
+        >
+          <MovieListItem
+            v-for="movie in movies"
+            :key="movie.id"
+            :movie="movie"
+          />
+        </Flicking>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -24,5 +41,20 @@ export default {
 <style>
 .article-list {
   text-align: start;
+}
+
+.title {
+  letter-spacing: 0px;
+  background: linear-gradient(
+    45deg,
+    deepskyblue,
+    deeppink,
+    deepskyblue,
+    deeppink
+  );
+  background-clip: text;
+  -webkit-background-clip: text;
+  color: transparent;
+  font-size: 40px;
 }
 </style>
