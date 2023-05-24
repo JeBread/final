a1938d737a872a6c5b848337d1afff9ca0fae501
상제 <sangje112@gmail.com>
2023-05-25 08:44:40 +0900

    게시글 디테일 & 댓글 폼

--- final_front/src/components/ArticleList.vue
+++ final_front/src/components/ArticleList.vue
@@ -1,10 +1,26 @@
 <template>
-  <div class="article-list justify-content-center mt-5">
-    <ArticleListItem
-      v-for="article in articles"
-      :key="article.id"
-      :article="article"
-    />
+  <!-- <div class="article-list justify-content-center mt-5"> -->
+  <div class="justify-content-center align-content-center mt-5">
+    <div class="relative overflow-x-auto">
+      <table class="w-full text-sm text-left dark:text-gray-400">
+        <thead
+          class="text-xs uppercase bg-gray-900 bg-opacity-60 dark:bg-gray-700"
+        >
+          <tr>
+            <th scope="col" class="px-6 py-3 text-lg rounded-l-lg">
+              게시글 제목
+            </th>
+            <th scope="col" class="px-6 py-3 text-lg">작성자</th>
+            <th scope="col" class="px-6 py-3 text-lg rounded-r-lg">좋아요</th>
+          </tr>
+        </thead>
+        <ArticleListItem
+          v-for="article in articles"
+          :key="article.id"
+          :article="article"
+        />
+      </table>
+    </div>
   </div>
 </template>
 
